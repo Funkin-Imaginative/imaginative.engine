e391c0d9fd3d1999e7cb392b642a10751649dcb4
AtlasGamer27 <timeyougamemine@gmail.com>
2023-04-01 14:42:40 -0400

    RODS PULL

---
+++ source/MusicBeatState.hx
@@ -0,0 +1,21 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.FlxG;
+import flixel.text.FlxText;
+
+import flixel.tweens.FlxTween;
+class MainMenuState extends FlxState {
+	override public function create() {
+
+        // Create Menu Items Here
+
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float) {
+		super.update(elapsed);
+	}
+}
