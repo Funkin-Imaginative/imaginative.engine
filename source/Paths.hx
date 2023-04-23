e6c9745fa3fef6c8340d8ec04c94f4afadc4e7ee
Rodney, An Imaginative Person <59747809+RodneyAnImaginativePerson@users.noreply.github.com>
2023-04-23 03:05:49 -0400

    update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -191,7 +191,7 @@ class Paths {
 		return file;
 	}
 
-	inline static public function voices(song:String):Any {
+	inline static public function voices(song:String, suffix:):Any {
 		#if html5 return 'songs:assets/songs/${formatToSongPath(song)}/Voices.$SOUND_EXT'; #else
 		var songKey:String = '${formatToSongPath(song)}/Voices';
 		var voices = returnSound('songs', songKey);
