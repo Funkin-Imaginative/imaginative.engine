03df376dc2cabd6651a0d64a44588a73e831cac8
Rodney, An Imaginative Person <59747809+RodneyAnImaginativePerson@users.noreply.github.com>
2023-06-05 10:02:05 -0400

    update Note.hx

--- source/Note.hx
+++ source/Note.hx
@@ -31,6 +31,7 @@ class Note extends FlxSprite
 	public var altNote:Bool = false;
 	// public var invisNote:Bool = false;
 	public var isPixel:Bool = false;
+	public var pixelScale:Float = 6;
 
 	public var sustainLength:Float = 0;
 	public var isSustainNote:Bool = false;
@@ -41,14 +42,16 @@ class Note extends FlxSprite
 	public static var swagWidth:Float = 160 * 0.7;
 	public static var nameArray:Array<String> = ['left', 'down', 'up', 'right'];
 
-	public function new(strumTime:Float, noteData:Int, ?prevNote:Note, ?sustainNote:Bool = false) {
+	public function new(strumTime:Float, noteData:Int, /*pixelStuff:Array<String>,*/ ?prevNote:Note, ?sustainNote:Bool = false) {
 		super();
-
+		
 		if (prevNote == null) prevNote = this;
-
 		this.prevNote = prevNote;
 		isSustainNote = sustainNote;
-
+		
+		/*isPixel = pixelStuff[0];
+		pixelScale = pixelStuff[1];*/
+		
 		x += 50;
 		// MAKE SURE ITS DEFINITELY OFF SCREEN?
 		y -= 2000;
@@ -57,39 +60,23 @@ class Note extends FlxSprite
 		this.noteData = noteData;
 
 		if (isPixel) {
-			loadGraphic(Paths.image('weeb/pixelUI/arrows-pixels'), true, 17, 17);
-			
-			animation.add('${nameArray[noteData]}Scroll', [noteData + 4]);
-			/*animation.add('leftScroll', [4]);
-			animation.add('downScroll', [5]);
-			animation.add('upScroll', [6]);
-			animation.add('rightScroll', [7]);*/
-
 			if (isSustainNote) {
 				loadGraphic(Paths.image('weeb/pixelUI/arrowEnds'), true, 7, 6);
-
 				animation.add('${nameArray[noteData]}holdend', [noteData + 4]);
-				/*animation.add('leftholdend', [4]);
-				animation.add('upholdend', [6]);
-				animation.add('rightholdend', [7]);
-				animation.add('downholdend', [5]);*/
-
 				animation.add('${nameArray[noteData]}hold', [noteData]);
-				/*animation.add('lefthold', [0]);
-				animation.add('downhold', [1]);
-				animation.add('uphold', [2]);
-				animation.add('righthold', [3]);*/
+			} else {
+				loadGraphic(Paths.image('weeb/pixelUI/arrows-pixels'), true, 17, 17);
+				animation.add('${nameArray[noteData]}Scroll', [noteData + 4]);
 			}
-
-			setGraphicSize(Std.int(width * PlayState.daPixelZoom));
+			setGraphicSize(Std.int(width * pixelScale));
 			updateHitbox();
 			antialiasing = false;
 		} else {
 			frames = Paths.getSparrowAtlas('NOTE_assets');
-			animation.addByPrefix('${nameArray[noteData]}Scroll', '${nameArray[noteData]} static');
-			animation.addByPrefix('${nameArray[noteData]}holdend', '${nameArray[noteData]} hold end');
-			animation.addByPrefix('${nameArray[noteData]}hold', '${nameArray[noteData]} hold piece');
-
+			if (isPixel) {
+				animation.addByPrefix('${nameArray[noteData]}holdend', '${nameArray[noteData]} hold end');
+				animation.addByPrefix('${nameArray[noteData]}hold', '${nameArray[noteData]} hold piece');
+			} else animation.addByPrefix('${nameArray[noteData]}Scroll', '${nameArray[noteData]} static');
 			setGraphicSize(Std.int(width * 0.7));
 			updateHitbox();
 			antialiasing = true;
@@ -98,20 +85,6 @@ class Note extends FlxSprite
 		colorSwap = new ColorSwap();
 		shader = colorSwap.shader;
 
-		// switch (noteData) {
-		// 	case 0:
-		// 		x += swagWidth * 0;
-		// 		animation.play('leftScroll');
-		// 	case 1:
-		// 		x += swagWidth * 1;
-		// 		animation.play('downScroll');
-		// 	case 2:
-		// 		x += swagWidth * 2;
-		// 		animation.play('upScroll');
-		// 	case 3:
-		// 		x += swagWidth * 3;
-		// 		animation.play('rightScroll');
-		// }
 		x += swagWidth * noteData;
 		animation.play('${nameArray[noteData]}Scroll');
 
@@ -121,42 +94,17 @@ class Note extends FlxSprite
 			noteScore * 0.2;
 			alpha = 0.6;
 
-			if (PreferencesMenu.getPref('downscroll'))
-				angle = 180;
-
+			if (PreferencesMenu.getPref('downscroll')) angle = 180;
 			x += width / 2;
 
-			// switch (noteData) {
-			// 	case 2:
-			// 		animation.play('upholdend');
-			// 	case 3:
-			// 		animation.play('rightholdend');
-			// 	case 1:
-			// 		animation.play('downholdend');
-			// 	case 0:
-			// 		animation.play('leftholdend');
-			// }
 			animation.play('${nameArray[noteData]}holdend');
-
 			updateHitbox();
 
 			x -= width / 2;
-
 			if (isPixel) x += 30;
 
 			if (prevNote.isSustainNote) {
-				// switch (prevNote.noteData) {
-				// 	case 0:
-				// 		prevNote.animation.play('lefthold');
-				// 	case 1:
-				// 		prevNote.animation.play('downhold');
-				// 	case 2:
-				// 		prevNote.animation.play('uphold');
-				// 	case 3:
-				// 		prevNote.animation.play('righthold');
-				// }
 				prevNote.animation.play('${nameArray[noteData]}hold');
-
 				prevNote.scale.y *= Conductor.stepCrochet / 100 * 1.5 * PlayState.SONG.speed;
 				prevNote.updateHitbox();
 				// prevNote.setGraphicSize();
@@ -174,8 +122,7 @@ class Note extends FlxSprite
 				canBeHit = false;
 			} else {
 				if (strumTime > Conductor.songPosition - Conductor.safeZoneOffset) { // The * 0.5 is so that it's easier to hit them too late, instead of too early
-					if (strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * 0.5))
-						canBeHit = true;
+					if (strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * 0.5)) canBeHit = true;
 				} else {
 					canBeHit = true;
 					willMiss = true;
@@ -183,12 +130,8 @@ class Note extends FlxSprite
 			}
 		} else {
 			canBeHit = false;
-
 			if (strumTime <= Conductor.songPosition) wasGoodHit = true;
 		}
-
-		if (tooLate) {
-			if (alpha > 0.3) alpha = 0.3;
-		}
+		if (tooLate) if (alpha > 0.3) alpha = 0.3;
 	}
 }
