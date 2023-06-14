27c6df97e88dfe2aab9a9c1e6432c96111f90892
Rodney, An Imaginative Person <59747809+RodneyAnImaginativePerson@users.noreply.github.com>
2023-06-13 23:34:00 -0400

    update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -5,59 +5,35 @@ import flixel.FlxSprite;
 using StringTools;
 
 class HealthIcon extends FlxSprite {
-	/**
-	 * Used for FreeplayState! If you use it elsewhere, prob gonna annoying
-	 */
-	public var sprTracker:FlxSprite;
-
-	var char:String = '';
-	var isPlayer:Bool = false;
-
-	public function new(char:String = 'bf', isPlayer:Bool = false)
-	{
+	public var sprTracker:FlxSprite; // Used for FreeplayState! If you use it elsewhere, prob gonna annoying
+	public var iconName(default, set):String = 'face';
+	public function new(icon:String = 'face') {
 		super();
-
-		this.isPlayer = isPlayer;
-
-		changeIcon(char);
-		antialiasing = true;
+		iconName = icon;
 		scrollFactor.set();
 	}
 
-	public var isOldIcon:Bool = false;
-
-	public function swapOldIcon():Void
-	{
+	public var isOldIcon(default, set):Bool = false;
+	private function set_isOldIcon():Bool {
 		isOldIcon = !isOldIcon;
-
-		if (isOldIcon)
-			changeIcon('bf-old');
-		else
-			changeIcon(PlayState.chartData.player1);
+		if (isOldIcon) iconName = 'bf-old';
+		else iconName = iconName;
 	}
 
-	public function changeIcon(newChar:String):Void
-	{
-		if (newChar != 'bf-pixel' && newChar != 'bf-old')
-			newChar = newChar.split('-')[0].trim();
-
-		if (newChar != char)
-		{
-			if (animation.getByName(newChar) == null)
-			{
-				loadGraphic(Paths.image('icons/icon-' + newChar), true, 150, 150);
-				animation.add(newChar, [0, 1], 0, false, isPlayer);
-			}
-			animation.play(newChar);
-			char = newChar;
+	private function set_iconName(value:String):String {
+		if (iconName != value) {
+			loadGraphic(Paths.image('icons/' + iconName))
+			loadGraphic(Paths.image('icons/' + iconName), true, Math.floor(width / 2), Math.floor(height));
+			animation.add('idle', [0, 1], 0, false);
+			animation.play('idle');
+			antialiasing = !value.endsWith('-pixel');
+			if (iconName != 'bf-old') iconName = value;
 		}
+		return value;
 	}
 
-	override function update(elapsed:Float)
-	{
+	override function update(elapsed:Float) {
 		super.update(elapsed);
-
-		if (sprTracker != null)
-			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+		if (sprTracker != null) setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
 	}
 }
