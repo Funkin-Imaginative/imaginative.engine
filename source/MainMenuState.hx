c6975e93a042580e259f20de090db69b2b39f98c
AtlasGamer27 <timeyougamemine@gmail.com>
2023-09-11 10:03:18 -0400

    Removed Kickstarter (also commented credits)

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -92,11 +92,10 @@ class MainMenuState extends MusicBeatState
 		#if CAN_OPEN_LINKS
 		var hasPopupBlocker = #if web true #else false #end;
 
-		if (VideoState.seenVideo)
-			menuItems.createItem('kickstarter', selectDonate, hasPopupBlocker);
-		else
-			menuItems.createItem('donate', selectDonate, hasPopupBlocker);
 		#end
+
+		//menuItems.createItem('credits', function () startExitState(new OptionsState()));
+
 		menuItems.createItem('options', function() startExitState(new OptionsState()));
 
 		// center vertically
@@ -136,23 +135,6 @@ class MainMenuState extends MusicBeatState
 	{
 		camFollow.setPosition(selected.getGraphicMidpoint().x, selected.getGraphicMidpoint().y);
 	}
-
-	#if CAN_OPEN_LINKS
-	function selectDonate()
-	{
-		#if linux
-		// Sys.command('/usr/bin/xdg-open', ["https://ninja-muffin24.itch.io/funkin", "&"]);
-		Sys.command('/usr/bin/xdg-open', [
-			"https://www.kickstarter.com/projects/funkin/friday-night-funkin-the-full-ass-game/",
-			"&"
-		]);
-		#else
-		// FlxG.openURL('https://ninja-muffin24.itch.io/funkin');
-
-		FlxG.openURL('https://www.kickstarter.com/projects/funkin/friday-night-funkin-the-full-ass-game/');
-		#end
-	}
-	#end
 
 	public function openPrompt(prompt:Prompt, onClose:Void->Void)
 	{
