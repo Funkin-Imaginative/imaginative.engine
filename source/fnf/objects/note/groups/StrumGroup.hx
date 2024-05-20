ad9631bacef759ce3ca7864a958083c9530f2f92
Rodney, An Imaginative Person <59747809+RodneyAnImaginativePerson@users.noreply.github.com>
2024-05-20 10:45:40 -0400

    Update StrumGroup.hx

--- source/fnf/objects/note/groups/StrumGroup.hx
+++ source/fnf/objects/note/groups/StrumGroup.hx
@@ -74,9 +74,8 @@ class StrumGroup extends FlxTypedGroup<Strum> {
 	public var vocals:FlxSound;
 	public var vocalsFinished:Bool = false;
 
-	public function new(x:Float, y:Float, pixel:Bool = false) {
+	public function new(x:Float, y:Float, pixel:Bool = false, amount:Int = 4) {
 		super();
-		var amount:Int = 4;
 		for (i in 0...amount) {
 			var babyArrow:Strum = new Strum(x - (Note.swagWidth / 2), y, i, pixel);
 			babyArrow.x += Note.swagWidth * i;
