86a4179943442be38886c20d813503d209f45beb
Rodney, An Imaginative Furball <59747809+rodney528@users.noreply.github.com>
2024-08-29 01:50:21 -0400

    update typedefs.hx

--- concepts/typedefs.hx
+++ concepts/typedefs.hx
@@ -1,12 +1,102 @@
-typedef TJsonCharacterSection = {
-	var position:PositionMeta;
-	var camera:PositionMeta;
+typedef TypeXY<Type> = {
+	var x:Type;
+	var y:Type;
+}
+typedef Dimensions = {
+	var width:Int;
+	var height:Int;
+}
+typedef AssetTyping = {
+	var image:String;
+	var type:String;
+}
+typedef AnimationTyping = {
+	var asset:AssetTyping;
+	var name:String;
+	var tag:String;
+	var dimensions:Dimensions;
+	var indices:Array<Int>;
+	var offset:TypeXY<Float>;
+	var flip:TypeXY<Bool>;
+	var loop:Bool;
+	var fps:Int;
+}
+typedef AllowedModesTyping = {
+	var playAsEnemy:Bool;
+	var p2AsEnemy:Bool;
+}
+typedef CheckpointTyping = {
+	var time:Float;
+	var bpm:Float
+	var measure:Array<Float>;
+}
+
+typedef CharacterSection = {
+	var camera:TypeXY<Float>;
 	var color:String;
 	var icon:String;
-	@:optional var singlength:Float;
+	var singlength:Float;
 }
-typedef TJsonSpriteSection = {}
-typedef TJsonBopperSection = {
-	@:optional var invertal:Int;
-	@:optional var skipnegative:Bool;
+typedef BeatSection = {
+	var invertal:Int;
+	var skipnegative:Bool;
+}
+typedef OffsetsSection = {
+	var position:TypeXY<Float>;
+	var flip:TypeXY<Bool>;
+	var scale:TypeXY<Float>;
+}
+typedef SpriteSection = {
+	> CharacterSection;
+	> BeatSection;
+	> OffsetsSection;
+	var animations:Array<AnimationTyping>;
+	var antialiasing:Bool;
+	var flip:TypeXY<Bool>;
+	var scale:TypeXY<Float>;
+}
+
+typedef LevelSection = {
+	var title:String;
+	var songs:String;
+	var startingDiff:Int;
+	var difficulties:Array<String>;
+	var characters:Array<String>;
+	var color:String;
+}
+typedef DifficultySection = {
+	var display:String;
+	var variant:Null<String>;
+	var scoreMult:Float;
+}
+typedef SongSection = {
+	var name:String;
+	var icon:String;
+	var startingDiff:Int;
+	var difficulties;Array<String>;
+	var color:String;
+	var allowedModes:AllowedModesTyping;
+}
+typedef AudioSection = {
+	var artist:String;
+	var name:String;
+	var bpm:Float;
+	var measure:Array<Float>;
+	var checkpoints:Array<CheckpointTyping>;
+	var offset:Float;
+}
+typedef MetaSection = {
+	> LevelSection;
+	> DifficultySection;
+	> SongSection;
+	> AudioSection;
+}
+typedef ExtraSection = {
+	var name:String;
+	var data:Dynamic;
+}
+
+typedef CharacterData = {
+	var sprite:SpriteSection;
+	var extra:Array<ExtraSection>;
 }
