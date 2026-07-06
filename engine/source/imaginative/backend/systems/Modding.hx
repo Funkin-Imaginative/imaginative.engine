49fb4eeaf2cf2cba3062cb56be875998df3f213f
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2026-07-06 03:25:30 -0400

    how tf did i miss this

--- engine/source/imaginative/backend/systems/Modding.hx
+++ engine/source/imaginative/backend/systems/Modding.hx
@@ -5,21 +5,21 @@ class Modding {
 	/**
 	 * The current master mod.
 	 */
-	public var masterMod(default, null):Null<String> = null;
+	public static var masterMod(default, null):Null<String> = null;
 	/**
 	 * The current module mod.
 	 */
-	public var moduleMod:Null<String> = null;
+	public static var moduleMod:Null<String> = null;
 
 	/**
 	 * The active module mods.
 	 */
-	public var moduleList:Array<String> = [];
+	public static var moduleList:Array<String> = [];
 
 	/**
 	 * If true, the current master mod is the engines fallback mod.
 	 */
-	public var masterIsFallback(get, never):Bool;
+	public static var masterIsFallback(get, never):Bool;
 	inline static function get_masterIsFallback():Bool
 		return masterMod == Game.fallbackMod;
 	#end
